c7853896b3e51f8ac9c980005d55cf76982ed8bc
kidiga <juniorprogrammer09@gmail.com>
2025-02-07 07:02:31 +0300

    modularized the name composable(center of top bar) to be reused

--- app/src/main/java/io/github/kdbrian/pseudo/features/home/composables/HomeTopAppBar.kt
+++ app/src/main/java/io/github/kdbrian/pseudo/features/home/composables/HomeTopAppBar.kt
@@ -5,23 +5,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.AccountCircle
 import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import io.github.kdbrian.pseudo.ui.composables.NestedRootName
 import io.github.kdbrian.pseudo.ui.nav.LocalDefaultBackgroundBrush
 import io.github.kdbrian.pseudo.ui.theme.PseudoTheme
-import io.github.kdbrian.pseudo.ui.theme.appDark
-import io.github.kdbrian.pseudo.ui.theme.greishWhite
 
 @Composable
 fun HomeTopAppBar(modifier: Modifier = Modifier) {
@@ -38,15 +34,7 @@ fun HomeTopAppBar(modifier: Modifier = Modifier) {
             Icon(imageVector = Icons.Rounded.Menu, contentDescription = null)
         }
 
-        Text(
-            text = "pseudonotes.dev",
-            modifier = Modifier
-                .background(shape = RoundedCornerShape(16.dp), color = greishWhite)
-                .padding(8.dp),
-            style = LocalTextStyle.current.copy(
-                color = appDark
-            )
-        )
+        NestedRootName()
 
 
         IconButton(onClick = {}) {
---
+++ app/src/main/java/io/github/kdbrian/pseudo/ui/composables/NestedRootName.kt
@@ -0,0 +1,35 @@
+package io.github.kdbrian.pseudo.ui.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.github.kdbrian.pseudo.ui.theme.appDark
+import io.github.kdbrian.pseudo.ui.theme.greishWhite
+
+@Composable
+fun NestedRootName(
+    modifier: Modifier = Modifier,
+    name: String = "pseudonotes.dev",
+) {
+    Text(
+        text = name,
+        modifier = modifier
+            .background(shape = RoundedCornerShape(16.dp), color = greishWhite)
+            .padding(8.dp),
+        style = LocalTextStyle.current.copy(
+            color = appDark
+        )
+    )
+}
+
+@Preview
+@Composable
+private fun NestedRootNamePrev() {
+
+}
