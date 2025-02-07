b59cb385716232472017c17d36d45ae4366340d8
kidiga <juniorprogrammer09@gmail.com>
2025-02-07 06:55:22 +0300

    finished the recent item, with an extra action place to insert custom composables depending on the state of the data in the current recent item

--- app/src/main/java/io/github/kdbrian/pseudo/features/catalogue/RecentItem.kt
+++ app/src/main/java/io/github/kdbrian/pseudo/features/catalogue/RecentItem.kt
@@ -3,19 +3,33 @@ package io.github.kdbrian.pseudo.features.catalogue
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
-import io.github.kdbrian.pseudo.ui.nav.LocalDefaultBackgroundBrush
+import androidx.compose.ui.unit.sp
+import io.github.kdbrian.pseudo.ui.nav.LocalTextStyle
 import io.github.kdbrian.pseudo.ui.theme.PseudoTheme
 import kotlinx.serialization.Serializable
+import kotlin.random.Random
 
 @Serializable
 data class RecentItemModel(
@@ -24,8 +38,25 @@ data class RecentItemModel(
 
 
 @Composable
-fun RecentItem(modifier: Modifier = Modifier) {
-
+fun RecentItem(
+    text: String = LoremIpsum(Random.nextInt(3, 6)).values.joinToString(),
+    modifier: Modifier = Modifier,
+    action: @Composable () -> Unit = {},
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = text,
+            style = LocalTextStyle.current.copy(
+                fontSize = 14.sp
+            )
+        )
+        Spacer(Modifier.weight(1f))
+        action()
+    }
 }
 
 @Composable
@@ -34,9 +65,21 @@ fun RecentItemLoading(modifier: Modifier = Modifier) {
         modifier = modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.spacedBy(4.dp)
     ){
-        Spacer(Modifier.fillMaxWidth(1f).height(12.dp).background(color = Color.LightGray, shape = RoundedCornerShape(6.dp)).padding(4.dp))
+        Spacer(
+            Modifier
+                .fillMaxWidth(1f)
+                .height(12.dp)
+                .background(color = Color.LightGray, shape = RoundedCornerShape(6.dp))
+                .padding(4.dp)
+        )
 
-        Spacer(Modifier.fillMaxWidth(0.60f).height(12.dp).background(color = Color.LightGray, shape = RoundedCornerShape(6.dp)).padding(4.dp))
+        Spacer(
+            Modifier
+                .fillMaxWidth(0.60f)
+                .height(12.dp)
+                .background(color = Color.LightGray, shape = RoundedCornerShape(6.dp))
+                .padding(4.dp)
+        )
     }
 }
 
@@ -44,7 +87,20 @@ fun RecentItemLoading(modifier: Modifier = Modifier) {
 @Composable
 private fun RecentItemPrev() {
     PseudoTheme {
-//        RecentItem()
-        RecentItemLoading()
+        RecentItem(
+            modifier = Modifier.padding(horizontal = 6.dp),
+            action = {
+                TextButton (onClick = {}) {
+                    Text(
+                        text = "Trending",
+                        style = LocalTextStyle.current.copy(
+                            color = Color.Red,
+                            fontWeight = FontWeight.Light,
+                            fontStyle = FontStyle.Italic
+                        )
+                    )
+                }
+            }
+        )
     }
 }
