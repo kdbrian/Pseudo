acbf5467ceef519fb2398bca7c993e86c1cd9eeb
kidiga <juniorprogrammer09@gmail.com>
2025-02-07 07:04:30 +0300

    centered the text in composable

--- app/src/main/java/io/github/kdbrian/pseudo/ui/composables/NestedRootName.kt
+++ app/src/main/java/io/github/kdbrian/pseudo/ui/composables/NestedRootName.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.github.kdbrian.pseudo.ui.theme.appDark
@@ -23,7 +24,8 @@ fun NestedRootName(
             .background(shape = RoundedCornerShape(16.dp), color = greishWhite)
             .padding(8.dp),
         style = LocalTextStyle.current.copy(
-            color = appDark
+            color = appDark,
+            textAlign = TextAlign.Center
         )
     )
 }
